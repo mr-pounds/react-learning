39ebc212cbbc4060606e04ca7952b1e1e0ea82ad
zzz <740870608@qq.com>
2022-04-01 13:26:03 +0800

    swiper案例

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './02-advance/04-banner-swiper';
+import App from './02-advance/04-swiper-react';
 
 ReactDOM.render(
   <App></App>,
